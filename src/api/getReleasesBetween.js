185c990b99bc1d92f07e27c7f8b6769da48a0376
Adonai <hawkins.dom@gmail.com>
2019-11-06 00:51:08 -0600

    Fixed logic, implemented tests

--- src/api/getReleasesBetween.js
+++ src/api/getReleasesBetween.js
@@ -5,31 +5,37 @@ import fetchGithubRelease from "./fetchGithubRelease";
  * @param {Object} options
  * @param {String} options.majorVersion - e.g: v12.4.8
  * @param {String} options.minorVersion - e.g: v10.11.9
- *
+ * @yields {Release}
  */
 export default async function* getReleasesBetween({
   majorVersion,
   minorVersion,
   repositoryUrl
 }) {
+  const majorVersionLowerCase = majorVersion
+    ? majorVersion.toLowerCase()
+    : null;
+  const minorVersionLowerCase = minorVersion
+    ? minorVersion.toLowerCase()
+    : null;
+
   const releaseIterator = fetchGithubRelease(repositoryUrl);
 
   let majorVersionRelease;
 
   for await (const release of releaseIterator) {
-    if (!majorVersion && !majorVersionRelease) {
-      majorVersionRelease = release;
-    }
-
-    if (release.tagName.toLowerCase() === minorVersion.toLowerCase()) {
-      return release;
-    }
-
-    if (
-      !majorVersionRelease &&
-      release.tagName.toLowerCase() !== majorVersion.toLowerCase()
-    ) {
-      continue;
+    if (!majorVersionRelease) {
+      if (
+        !majorVersionLowerCase ||
+        release.tagName.toLowerCase() === majorVersionLowerCase
+      ) {
+        majorVersionRelease = release;
+      } else {
+        continue;
+      }
+    } else if (release.tagName.toLowerCase() === minorVersionLowerCase) {
+      yield release;
+      break;
     }
 
     yield release;
---
+++ src/api/getReleasesBetween.spec.js
@@ -0,0 +1,126 @@
+import { expect } from "chai";
+import sinon from "sinon";
+import * as fetchGithubReleaseModule from "./fetchGithubRelease";
+import getReleasesBetween from "./getReleasesBetween";
+
+function stubAsyncGenerator(generatorModule, iteratorValues) {
+  return sinon.stub(generatorModule, "default").returns({
+    [Symbol.asyncIterator]() {
+      return {
+        i: 0,
+        next() {
+          if (this.i < iteratorValues.length) {
+            return Promise.resolve({
+              value: iteratorValues[this.i++],
+              done: false
+            });
+          }
+          return Promise.resolve({ done: true });
+        }
+      };
+    }
+  });
+}
+
+describe("getReleasesBetween", () => {
+  const mockReleases = [
+    { tagName: "v3.0.0" },
+    { tagName: "v2.9.0" },
+    { tagName: "v2.8.0" },
+    { tagName: "v2.7.0" }
+  ];
+
+  beforeEach(() => {
+    fetchGithubReleaseModule.default.restore &&
+      fetchGithubReleaseModule.default.restore();
+  });
+
+  it("should yield releases in range", async () => {
+    stubAsyncGenerator(fetchGithubReleaseModule, mockReleases);
+
+    const releaseIterator = getReleasesBetween({
+      majorVersion: "v2.9.0",
+      minorVersion: "v2.8.0",
+      repositoryUrl: "octocat/Hello-World"
+    });
+    const releasesInRange = [];
+    for await (const release of releaseIterator) {
+      releasesInRange.push(release);
+    }
+
+    expect(releasesInRange).to.eql([
+      { tagName: "v2.9.0" },
+      { tagName: "v2.8.0" }
+    ]);
+    expect(fetchGithubReleaseModule.default.calledOnce).to.be.true;
+    expect(fetchGithubReleaseModule.default.getCall(0).args[0]).to.equal(
+      "octocat/Hello-World"
+    );
+  });
+
+  it("should yield releases using latest major version", async () => {
+    stubAsyncGenerator(fetchGithubReleaseModule, mockReleases);
+
+    const releaseIterator = getReleasesBetween({
+      minorVersion: "v2.8.0",
+      repositoryUrl: "octocat/Hello-World"
+    });
+    const releasesInRange = [];
+    for await (const release of releaseIterator) {
+      releasesInRange.push(release);
+    }
+
+    expect(releasesInRange).to.eql([
+      { tagName: "v3.0.0" },
+      { tagName: "v2.9.0" },
+      { tagName: "v2.8.0" }
+    ]);
+    expect(fetchGithubReleaseModule.default.calledOnce).to.be.true;
+    expect(fetchGithubReleaseModule.default.getCall(0).args[0]).to.equal(
+      "octocat/Hello-World"
+    );
+  });
+
+  it("should yield releases using oldest minor version", async () => {
+    stubAsyncGenerator(fetchGithubReleaseModule, mockReleases);
+
+    const releaseIterator = getReleasesBetween({
+      majorVersion: "v2.8.0",
+      repositoryUrl: "hawkeng/flash-dictionary"
+    });
+    const releasesInRange = [];
+    for await (const release of releaseIterator) {
+      releasesInRange.push(release);
+    }
+
+    expect(releasesInRange).to.eql([
+      { tagName: "v2.8.0" },
+      { tagName: "v2.7.0" }
+    ]);
+    expect(fetchGithubReleaseModule.default.calledOnce).to.be.true;
+    expect(fetchGithubReleaseModule.default.getCall(0).args[0]).to.equal(
+      "hawkeng/flash-dictionary"
+    );
+  });
+
+  it("should yield all releases", async () => {
+    stubAsyncGenerator(fetchGithubReleaseModule, mockReleases);
+
+    const releaseIterator = getReleasesBetween({
+      repositoryUrl: "hawkeng/b-forums"
+    });
+    const releasesInRange = [];
+    for await (const releases of releaseIterator) {
+      releasesInRange.push(releases);
+    }
+
+    expect(releasesInRange).to.eql(mockReleases);
+    expect(fetchGithubReleaseModule.default.calledOnce).to.be.true;
+    expect(fetchGithubReleaseModule.default.getCall(0).args[0]).to.equal(
+      "hawkeng/b-forums"
+    );
+  });
+
+  // TODO: Implement test case
+  // it("should throw on invalid repository url");
+});
