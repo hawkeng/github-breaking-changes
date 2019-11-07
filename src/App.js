5ed5658bd445b6191ab56a6b203bde51cdcf5a01
Adonai <hawkins.dom@gmail.com>
2019-11-06 22:48:01 -0600

    Uses AppStateProvider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { ApolloProvider } from "@apollo/react-hooks";
-
-import apolloClient from "./apollo-client";
+// import { ApolloProvider } from "@apollo/react-hooks";
 import "./App.css";
+// import apolloClient from "./apollo-client";
 import ReleaseForm from "./components/ReleaseForm";
+import { AppStateProvider } from "./contexts/AppStateContext";
 
 function App() {
   return (
-    <ApolloProvider client={apolloClient}>
+    // <ApolloProvider client={apolloClient}>
+    <AppStateProvider>
       <div className="spaced container">
         <h1 className="title is-1">Check breaking changes between versions</h1>
         <ReleaseForm />
       </div>
-    </ApolloProvider>
+    </AppStateProvider>
+    // </ApolloProvider>
   );
 }
 
