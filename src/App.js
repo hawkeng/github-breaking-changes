33038e2cb3db493401a7d5e808bc13be2cbce9b2
Adonai <hawkins.dom@gmail.com>
2019-11-13 23:42:28 -0600

    Implemented breaking changes table

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from "react";
-// import { ApolloProvider } from "@apollo/react-hooks";
 import "./App.css";
-// import apolloClient from "./apollo-client";
 import ReleaseForm from "./components/ReleaseForm";
+import BreakingChangesTable from "./components/BreakingChangesTable";
 import { AppStateProvider } from "./contexts/AppStateContext";
 
 function App() {
   return (
-    // <ApolloProvider client={apolloClient}>
     <AppStateProvider>
       <div className="spaced container">
         <h1 className="title is-1">Check breaking changes between versions</h1>
         <ReleaseForm />
+        <BreakingChangesTable />
       </div>
     </AppStateProvider>
-    // </ApolloProvider>
   );
 }
 
--- src/components/BreakingChangesTable.jsx
+++ src/components/BreakingChangesTable.jsx
@@ -1,8 +1,63 @@
 import React, { useContext } from "react";
-import AppStateContext from "../contexts/AppStateContext";
+import ReactMarkdown from "react-markdown";
+import { AppStateContext } from "../contexts/AppStateContext";
+import useGithubReleasesWithBreakingChanges from "../hooks/useGithubReleasesWithBreakingChanges";
+
+const ReleaseRow = ({ release }) => {
+  return (
+    <tr>
+      <td>
+        <a href={release.url}>{release.tagName}</a>
+      </td>
+      <td>{release.createdAt}</td>
+      <td>
+        <ReactMarkdown source={release.breakingChanges} />
+      </td>
+    </tr>
+  );
+};
 
 const BreakingChangesList = () => {
   const { state: appState } = useContext(AppStateContext);
+  const { repository, minorVersion, majorVersion } = appState.request
+    ? appState.request
+    : {};
+  const { releases, loading, error } = useGithubReleasesWithBreakingChanges({
+    pageSize: 25,
+    repository,
+    between: { majorVersion, minorVersion }
+  });
+
+  if (!repository) {
+    return null;
+  }
+
+  if (loading) {
+    return "Loading...";
+  }
+
+  if (error) {
+    console.log(error);
+    return error.message;
+  }
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Tag</th>
+          <th>Created</th>
+          <th>Breaking Changes</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {releases.map(release => (
+          <ReleaseRow key={release.tagName} release={release} />
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default BreakingChangesList;
