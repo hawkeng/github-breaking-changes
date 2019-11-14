a35170dedaea03f52f258eb6e97e7110c8f110a6
Adonai <hawkins.dom@gmail.com>
2019-11-13 23:39:11 -0600

    Changed env variable to match create-react-app

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -1,6 +1,6 @@
 import ApolloClient from "apollo-boost";
 
-const githubAccessToken = process.env.GITHUB_ACCESS_TOKEN;
+const githubAccessToken = process.env.REACT_APP_GITHUB_ACCESS_TOKEN;
 
 const client = new ApolloClient({
   uri: "https://api.github.com/graphql",
