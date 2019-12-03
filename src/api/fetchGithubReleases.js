99da1cf0029861f697895ac773083e0af5c54d96
Adonai <hawkins.dom@gmail.com>
2019-12-02 21:22:21 -0600

    Implements http request and removes apollo

--- src/api/fetchGithubReleases.js
+++ src/api/fetchGithubReleases.js
@@ -1,37 +1,8 @@
 import get from "lodash.get";
-import { gql } from "apollo-boost";
-import apolloClient from "../apollo-client";
 import "../typedef";
 
-const FETCH_LIMIT_FOR_RECORDS = 100;
-
-const LATEST_RELEASES_QUERY = gql`
-  query LatestReleases(
-    $owner: String!
-    $repository: String!
-    $count: Int!
-    $cursor: String
-  ) {
-    repository(owner: $owner, name: $repository) {
-      releases(
-        first: $count
-        after: $cursor
-        orderBy: { field: CREATED_AT, direction: DESC }
-      ) {
-        totalCount
-        edges {
-          cursor
-          node {
-            tagName
-            createdAt
-            url
-            description
-          }
-        }
-      }
-    }
-  }
-`;
+const DATA_SOURCE =
+  "https://us-central1-github-breaking-changes-7f7d1.cloudfunctions.net/githubReleases";
 
 const getTotalCount = data => get(data, "repository.releases.totalCount", 0);
 
@@ -55,14 +26,13 @@ const getCursor = data => {
  * @param {Object} options
  * @param {String} options.repositoryUrl - In the form of <owner>/<repository>
  * @param {String} [options.cursor]
- * @param {String} [options.count=FETCH_LIMIT_FOR_RECORDS] - Number of records
- * to fetch
+ * @param {String} [options.count] - Number of records to fetch
  * @returns {GithubReleasesResult}
  */
 export default async function fetchGithubReleases({
   repositoryUrl,
   cursor,
-  count = FETCH_LIMIT_FOR_RECORDS
+  count
 } = {}) {
   let [owner, repository] = repositoryUrl.split("/");
   if (!owner || !repository) {
@@ -72,11 +42,16 @@ export default async function fetchGithubReleases({
   }
 
   try {
-    const { data } = await apolloClient.query({
-      query: LATEST_RELEASES_QUERY,
-      variables: { owner, repository, count, cursor },
-      fetchPolicy: "cache-first"
-    });
+    const rawParams = {
+      repository,
+      owner,
+      ...(count ? { count } : undefined),
+      ...(cursor ? { cursor } : undefined)
+    };
+    const params = new URLSearchParams(rawParams);
+
+    const response = await fetch(`${DATA_SOURCE}?${params}`);
+    const { data } = await response.json();
     return {
       data: getReleases(data),
       totalRecords: getTotalCount(data),
